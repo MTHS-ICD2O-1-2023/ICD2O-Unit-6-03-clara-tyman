ee26e4799ca6f62dbb051dc262c2473911523a8a
Clara <144264170+clara-tyman@users.noreply.github.com>
2024-05-22 18:48:31 +0000

    start : need to fix json stuff

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Clara Tyman All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Clara Tyman
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("ICD2O-Unit-6-03-clara-tyman/sw.js", {
+    scope: "/ICD2O-Unit-6-03-clara-tyman /",
+  })
+}
+
+"use strict"
+
+async function getTemperature() {
+  try {
+    const resultJSON = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
+    const jsonData = await resultJSON.json()
+    //console.log(jsonData)
+    const firstDataSet = jsonData[0]
+    const textDataSet = firstDataSet.text
+    console.log(textDataSet)
+  } catch (error) {
+    console.error(error)
+  }
+}
