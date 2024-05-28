6a1781ceecee65dab9fc62526f29693acd95603f
Clara <144264170+clara-tyman@users.noreply.github.com>
2024-05-28 18:23:39 +0000

    everything code +design items

--- js/script.js
+++ js/script.js
@@ -1,29 +1,35 @@
-// Copyright (c) 2024 Clara Tyman All rights reserved
-//
-// Created by: Clara
+// Created by: Clara Tyman
 // Created on: May 2024
 // This file contains the JS functions for index.html
 
+'use strict'
+
 /**
- * Check servie worker.
+ * Check service worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("ICD2O-Unit-6-03-clara-tyman/sw.js", {
-    scope: "/ICD2O-Unit-6-03-clara-tyman /",
+  navigator.serviceWorker.register("/ICS2O-Unit6-03-HTML/sw.js", {
+    scope: "/ICS2O-Unit6-03-HTML/",
   })
 }
 
-"use strict"
-
-async function getTemperature() {
+/**
+ * Get API for weather.
+*/
+const getWeather = async (URLAddress) => {
   try {
-    const resultJSON = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
-    const jsonData = await resultJSON.json()
-    //console.log(jsonData)
-    const firstDataSet = jsonData[0]
-    const textDataSet = firstDataSet.text
-    console.log(textDataSet)
-  } catch (error) {
-    console.error(error)
+    const result = await fetch(URLAddress)
+    const jsonData = await result.json()
+    console.log(jsonData.main.temp)
+    const temperature = jsonData.main.temp - 273.15
+    const symbol = jsonData.weather[0].icon
+    const description = jsonData.weather[0].description
+    document.getElementById("api-weather").innerHTML = "The current weather is " + temperature.toFixed(0) + "°C" + "<img src='https://openweathermap.org/img/wn/" + symbol + "@2x.png' alt='Weather Icon' width='15%'>" + "with " + description
+  } catch (err) {
+    console.log(err)
+    document.getElementById("api-weather").innerHTML = "Error fetching current weather."
   }
 }
+
+
+getWeather("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
